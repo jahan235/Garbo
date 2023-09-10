a13ef5d2b4ff986a61d080a1399a5d6023d8d1b4
Jahan <jahanzaibamail@gmail.com>
2023-09-10 06:04:48 -0400

    WOW! Second commit! :o

--- GARBO.py
+++ GARBO.py
@@ -1,14 +1,2 @@
-x = 0
-magicNumber = 5
-numberGuessed = False
-
-while not numberGuessed:
-    x = int(input("ENTER YOUR GUESS: "))
-    if x > magicNumber:
-        print("\nLOWER\n")
-    elif x < magicNumber:
-        print("\nHIGHER\n")
-    else:
-        numberGuessed = True
-
-print(f"YOU GUESSED CORRECTLY! THE MAGIC NUMBER WAS: {magicNumber}!")
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
